8e7d3c744e535038c3c4e85e2daab94e73192694
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2023-07-07 13:18:22 -0400

    jhjhjhjh

--- No Timer/src/jason/notimer/NoTimer.java
+++ No Timer/src/jason/notimer/NoTimer.java
@@ -32,7 +32,7 @@ public class NoTimer extends JFrame {
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-//no
+//no1
 	private void initGUI() {
 		TitleLabel titleLabel = new TitleLabel("My Timer");
 		add(titleLabel, BorderLayout.PAGE_START);
