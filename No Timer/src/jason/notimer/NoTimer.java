a1fcfec46023a59e623c5b6eb80a0a98a68081e9
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2023-07-07 13:33:12 -0400

    Merge branch 'master' of https://github.com/JasonH81/Timer.git

--- No Timer/src/jason/notimer/NoTimer.java
+++ No Timer/src/jason/notimer/NoTimer.java
@@ -22,7 +22,7 @@ public class NoTimer extends JFrame {
 	private NoTimerPanel timerPanel = new NoTimerPanel(0, font);
 	
 	public NoTimer() {
-		
+		//test
 		initGUI();
 		
 		setTitle("My Timer");
